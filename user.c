3e2e1e63e04f26f20640c98b67b5abd07faca3a8
Miguel <henriques.miguelpereira@gmail.com>
2026-09-19 17:32:13 +0100

    added tudo

--- user.c
+++ user.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,8 +12,12 @@
 #define DEFAULT_DSIP "193.136.138.142"
 #define DEFAULT_DSPORT 59000
 
-// Processar os argumentos
-int parse_arguments(int argc, char *argv[], int *peerport, char **dsip, int *dsport){
+
+
+
+int parse_arguments(int argc, char *argv[],
+                    int *peerport, char **dsip, int *dsport)
+{
     for (int i = 1; i < argc; i++) {
 
         if (strcmp(argv[i], "-m") == 0) {
@@ -46,75 +51,299 @@ int parse_arguments(int argc, char *argv[], int *peerport, char **dsip, int *dsp
             }
 
         } else {
+
             printf("Erro: argumento desconhecido: %s\n", argv[i]);
             return 0;
         }
     }
 
-    // Verificar se a peerport foi fornecida
     if (*peerport == -1) {
         printf("Erro: a peerport (-m) é obrigatória.\n");
         return 0;
     }
 
-    // Mostrar os valores obtidos
     printf("peerport = %d\n", *peerport);
     printf("DSIP     = %s\n", *dsip);
     printf("DSport   = %d\n", *dsport);
+
     return 1;
 }
 
-int communicate(int sockfd, struct sockaddr_in *server_addr, const char *msg, char *response, size_t resp_len) {
+
+
+
+int communicate(int sockfd,
+                struct sockaddr_in *server_addr,
+                const char *msg,
+                char *response,
+                size_t resp_len)
+{
     socklen_t addr_len = sizeof(*server_addr);
 
-    if(sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr*)server_addr, addr_len) < 0){
+    /* Enviar mensagem para o DS */
+    if (sendto(sockfd,
+               msg,
+               strlen(msg),
+               0,
+               (struct sockaddr *)server_addr,
+               addr_len) < 0)
+    {
         perror("sendto");
         return -1;
     }
 
-    ssize_t bytes_recvd = recvfrom(sockfd, response, resp_len - 1, 0, (struct sockaddr *)server_addr, &addr_len);
+    /* Receber resposta */
+    struct sockaddr_in sender_addr;
+    addr_len = sizeof(sender_addr);
 
-    if(bytes_recvd < 0) {
-        printf("Communication failure.\n");
+    ssize_t bytes_recvd = recvfrom(sockfd,
+                                   response,
+                                   resp_len - 1,
+                                   0,
+                                   (struct sockaddr *)&sender_addr,
+                                   &addr_len);
+
+    if (bytes_recvd < 0) {
+        perror("recvfrom");
         return -1;
     }
+
     response[bytes_recvd] = '\0';
+
     return 0;
 }
 
-void login(int sockfd, struct sockaddr_in *server_addr, const char *uid, const char *password, const char *tcpPort){
+
+
+
+int login(int sockfd,
+          struct sockaddr_in *server_addr,
+          const char *uid,
+          const char *password,
+          int peerport)
+{
     char message[128];
     char response[256];
 
-    snprintf(message, sizeof(message), "LIN %s %s %s\n", uid, password, tcpPort);
+    sprintf(message,
+            "LIN %s %s %d\n",
+            uid,
+            password,
+            peerport);
 
-    if(communicate(sockfd, server_addr, message, response, sizeof(response)) == 0){
+    if (communicate(sockfd,
+                    server_addr,
+                    message,
+                    response,
+                    sizeof(response)) == 0)
+    {
         char status[10];
-        if(sscanf(response, "RLI %s", status) == 1){ 
 
-            if(strcmp(status, "OK") == 0){
-                printf("Login successful.\n");
-            } else if(strcmp(status, "NOK") == 0){
-                printf("Login failed: Incorrect password.\n");
-            } else if(strcmp(status, "REG") == 0){
-                printf("User successfuly registered and logged in.\n");
-            } else{
-                printf("Login format error.\n");
+        if (sscanf(response, "RLI %9s", status) == 1) {
+
+            if (strcmp(status, "OK") == 0) {
+                printf("Successful login.\n");
+                return 1;
+
+            } else if (strcmp(status, "NOK") == 0) {
+                printf("Incorrect login attempt.\n");
+                return 0;
+
+            } else if (strcmp(status, "REG") == 0) {
+                printf("New user registered.\n");
+                return 1;
+
+            } else if (strcmp(status, "ERR") == 0) {
+                printf("Login request error.\n");
+                return 0;
+
+            } else {
+                printf("Unexpected login response.\n");
+                return 0;
             }
         }
     }
+
+    return 0;
 }
 
 
 
+int logout(int sockfd,
+           struct sockaddr_in *server_addr,
+           const char *uid,
+           const char *password)
+{
+    char message[128];
+    char response[256];
 
-int main(int argc, char *argv[]) {
+    sprintf(message,
+            "LOU %s %s\n",
+            uid,
+            password);
+
+    if (communicate(sockfd,
+                    server_addr,
+                    message,
+                    response,
+                    sizeof(response)) == 0)
+    {
+        char status[10];
+
+        if (sscanf(response, "RLO %9s", status) == 1) {
+
+            if (strcmp(status, "OK") == 0) {
+                printf("Successful logout.\n");
+                return 1;
+
+            } else if (strcmp(status, "NLG") == 0) {
+                printf("User not logged in.\n");
+
+            } else if (strcmp(status, "UNR") == 0) {
+                printf("Unknown user.\n");
+
+            } else if (strcmp(status, "WRP") == 0) {
+                printf("Wrong password.\n");
+
+            } else if (strcmp(status, "ERR") == 0) {
+                printf("Logout request error.\n");
+
+            } else {
+                printf("Unexpected logout response.\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+
+int unregister_user(int sockfd,
+                    struct sockaddr_in *server_addr,
+                    const char *uid,
+                    const char *password)
+{
+    char message[128];
+    char response[256];
+
+    sprintf(message,
+            "UNR %s %s\n",
+            uid,
+            password);
+
+    if (communicate(sockfd,
+                    server_addr,
+                    message,
+                    response,
+                    sizeof(response)) == 0)
+    {
+        char status[10];
+
+        if (sscanf(response, "RUR %9s", status) == 1) {
+
+            if (strcmp(status, "OK") == 0) {
+                printf("Successful unregister.\n");
+                return 1;
+
+            } else if (strcmp(status, "NOK") == 0) {
+                printf("User not logged in.\n");
+
+            } else if (strcmp(status, "UNR") == 0) {
+                printf("Unknown user.\n");
+
+            } else if (strcmp(status, "WRP") == 0) {
+                printf("Wrong password.\n");
+
+            } else if (strcmp(status, "ERR") == 0) {
+                printf("Unregister request error.\n");
+
+            } else {
+                printf("Unexpected unregister response.\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+
+int validar_UID(const char *UID)
+{
+    if (strlen(UID) != 6) {
+        return 0;
+    }
+
+    for (int i = 0; i < 6; i++) {
+
+        if (!isdigit((unsigned char)UID[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+int validar_Password(const char *Password)
+{
+    if (strlen(Password) != 8) {
+        return 0;
+    }
+
+    for (int i = 0; i < 8; i++) {
+
+        if (!isalnum((unsigned char)Password[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+
+int validar_Port(int Port)
+{
+    if (Port >= 1 && Port <= 65535) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
+int main(int argc, char *argv[])
+{
 
     int peerport = -1;
+
     char *dsip = DEFAULT_DSIP;
+
     int dsport = DEFAULT_DSPORT;
 
-    if (parse_arguments(argc, argv, &peerport, &dsip, &dsport) == 0) return 1;
+    if (parse_arguments(argc,
+                        argv,
+                        &peerport,
+                        &dsip,
+                        &dsport) == 0)
+    {
+        return 1;
+    }
+
+
+    if (!validar_Port(peerport)) {
+        printf("Erro: peerport inválida.\n");
+        return 1;
+    }
+
+
+    if (!validar_Port(dsport)) {
+        printf("Erro: DSport inválida.\n");
+        return 1;
+    }
 
     int sockfd;
 
@@ -127,21 +356,34 @@ int main(int argc, char *argv[]) {
 
     printf("Socket UDP criado com sucesso.\n");
 
+
     struct sockaddr_in server_addr;
 
     memset(&server_addr, 0, sizeof(server_addr));
 
     server_addr.sin_family = AF_INET;
+
     server_addr.sin_port = htons(dsport);
-    
-    if (inet_pton(AF_INET, dsip, &server_addr.sin_addr) <= 0) {
+
+
+    if (inet_pton(AF_INET,
+                  dsip,
+                  &server_addr.sin_addr) <= 0)
+    {
         printf("Erro: endereço IP inválido.\n");
+
         close(sockfd);
+
         return 1;
     }
 
     printf("Endereço do DS configurado com sucesso.\n");
 
+
+    int logged_in = 0;
+
+    char current_uid[20] = "";
+    char current_password[20] = "";
     char buffer[256];
 
     while (1) {
@@ -152,26 +394,168 @@ int main(int argc, char *argv[]) {
             break;
         }
 
+
+        /* Remover '\n' */
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (strcmp(buffer, "exit") == 0) {
-            break;
+
+        if (strlen(buffer) == 0) {
+            continue;
         }
+
+
+        if (strcmp(buffer, "exit") == 0) {
+
+            if (logged_in) {
+
+                printf("Please logout before exiting.\n");
+
+            } else {
+
+                break;
+            }
+
+            continue;
+        }
+
+
+        int fields;
 
         char command[20];
         char uid[20];
         char password[20];
-        char tcpPort[20];
+        char extra[20];
 
-        int fields = sscanf(buffer, "%19s %19s %19s %19s",
-                             command, uid, password, tcpPort);
+        command[0] = '\0';
+        uid[0] = '\0';
+        password[0] = '\0';
+        extra[0] = '\0';
 
-        if(strcmp(command, "login") == 0){
-            if(fields == 4){
-                login(sockfd, &server_addr, uid, password, tcpPort);
-            } else{
-                printf("Usage: login <UID> <Password> <peerTCPport>\n");
+
+        fields = sscanf(buffer,
+                        "%19s %19s %19s %19s",
+                        command,
+                        uid,
+                        password,
+                        extra);
+
+
+
+
+        if (strcmp(command, "login") == 0) {
+
+
+            if (fields != 3) {
+
+                printf("Usage: login <UID> <Password>\n");
+
+                continue;
             }
+
+
+            /* Validar UID */
+
+            if (!validar_UID(uid)) {
+
+                printf("Invalid UID.\n");
+
+                continue;
+            }
+
+
+            /* Validar password */
+
+            if (!validar_Password(password)) {
+
+                printf("Invalid password.\n");
+
+                continue;
+            }
+
+
+            /* Enviar login para o DS */
+
+            if (login(sockfd,
+                      &server_addr,
+                      uid,
+                      password,
+                      peerport))
+            {
+
+                logged_in = 1;
+
+                strcpy(current_uid, uid);
+
+                strcpy(current_password, password);
+            }
+        }
+
+
+        else if (strcmp(command, "logout") == 0) {
+
+            if (fields != 1) {
+
+                printf("Usage: logout\n");
+
+                continue;
+            }
+
+
+            if (logged_in) {
+
+                if (logout(sockfd,
+                           &server_addr,
+                           current_uid,
+                           current_password))
+                {
+                    logged_in = 0;
+
+                    current_uid[0] = '\0';
+
+                    current_password[0] = '\0';
+                }
+
+            } else {
+
+                printf("User not logged in.\n");
+            }
+        }
+
+
+        else if (strcmp(command, "unregister") == 0) {
+
+            if (fields != 1) {
+
+                printf("Usage: unregister\n");
+
+                continue;
+            }
+
+
+            if (logged_in) {
+
+                if (unregister_user(sockfd,
+                                    &server_addr,
+                                    current_uid,
+                                    current_password))
+                {
+                    logged_in = 0;
+
+                    current_uid[0] = '\0';
+
+                    current_password[0] = '\0';
+                }
+
+            } else {
+
+                printf("User not logged in.\n");
+            }
+        }
+
+
+        else {
+
+            printf("Unknown command.\n");
         }
     }
 
@@ -179,4 +563,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
